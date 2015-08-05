7c139ab297d44569ea3f09ed58442c862eb2e0bd
megamanics <vinayski@gmail.com>
2015-08-05 04:13:34 +0000

    add Instrument panel

--- trade.js
+++ trade.js
@@ -4,15 +4,17 @@
 	app.controller('myCtrl', function($scope) {
 
 		$scope.createAccount =	function() {
-			    OANDA.account.register('USD', function(accountoanda) {
-			        $scope.myaccount = accountoanda;
+			    OANDA.account.register('USD', function(response) {
+			        $scope.myaccount = response;
 			    	$scope.$apply();
 			    });
 		}
 
+		$scope.createAccount();
+
 		$scope.openMarketOrder = function(accountId) {
-				OANDA.order.open(accountId, currencyPair, 100, 'sell', 'market', {'trailingStop' : 100}, function(openMarketOrderResponse) {
-					$scope.order = openMarketOrderResponse;
+				OANDA.order.open(accountId, currencyPair, 100, 'sell', 'market', {'trailingStop' : 100}, function(response) {
+					$scope.order = response;
 					$scope.listTrade(accountId);
 					$scope.$apply();
 				}
@@ -26,4 +28,38 @@
         		}
         	);
 		}
+
+		$scope.getRate = function getRates() {
+        		OANDA.rate.quote([currencyPair], function(response) {
+        			$scope.rates = response;
+        			$scope.$apply();
+        		}
+        	);
+    	}
+
+    	$scope.getHistoricalRate = function getHistory() {
+        		OANDA.rate.history(currencyPair, {'granularity' : "S5", 'count' : "2"}, function(response) {
+        			$scope.historicalRates = response;
+        			$scope.$apply();
+        		}
+        	);
+    	}
+
+    	$scope.getTransactionList = function transactionList(accountId) {
+        		OANDA.transaction.list(accountId, {'ids' : '2347', 'instrument' : currencyPair}, function(response) {
+        			$scope.transList = response;
+        			$scope.$apply();
+        		}
+        	);
+    	}
+
+    	$scope.getInstruments = function getInstruments() {
+        		OANDA.rate.instruments(['pip'], [''],  function(response) {
+        			$scope.instruments = response.instruments;
+        			$scope.$apply();
+        		}
+        	);
+    	}
+
+    	$scope.getInstruments();
     });
